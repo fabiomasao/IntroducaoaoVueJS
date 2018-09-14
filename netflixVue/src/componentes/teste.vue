ff0d56ef058f9b5802199140f2eb840f6dc1938e
Fabio Masao Hosono <fabiomasao@hotmail.com>
2018-09-14 15:55:15 -0300

    Alterações em 14/09/2018

---
+++ netflixVue/src/componentes/teste.vue
@@ -0,0 +1,25 @@
+<template>
+	
+<h1>{{titulo}}</h1>
+
+</template>
+
+<script>
+export default {
+	name:'teste',
+	props:['titulo']
+
+}
+	
+
+</script>
+
+
+<style>
+h1{
+	color:red;
+}
+	
+
+
+</style>
